71d2b741c24da7383c3054622df29cabc18027d9
rusefi <rusefillc@gmail.com>
2020-04-11 16:27:32 -0400

    tools

---
+++ Special/Tools/git_merge_normal_git_into_wiki_git.bat
@@ -0,0 +1,9 @@
+git checkout -b wiki-git-local wiki-git/master          >> log.txt 2>>err.txt
+
+git fetch normal-git                                    >> log.txt 2>>err.txt
+git merge normal-git/master                             >> log.txt 2>>err.txt
+git push wiki-git HEAD:master                           >> log.txt 2>>err.txt
+
+git checkout master                                     >> log.txt 2>>err.txt
+git branch -d wiki-git-local                            >> log.txt 2>>err.txt
+
