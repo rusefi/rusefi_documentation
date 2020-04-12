8ee071436281128b28d51f1bee7ee8534f4b08d6
rusefi <rusefillc@gmail.com>
2020-04-12 12:02:13 -0400

    more crazy

--- Special/Tools/git_add_upstream.bat
+++ Special/Tools/git_add_upstream.bat
@@ -1,5 +1,6 @@
 git remote -v
 git remote add wiki-git https://github.com/rusefi/rusefi_documentation.wiki.git
+git remote add best-wiki-git https://github.com/rusefi/rusefi.wiki.git
 git remote add normal-git https://github.com/rusefi/rusefi_documentation.git
 git remote -v
 
