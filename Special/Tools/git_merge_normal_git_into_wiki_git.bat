be9c89b3ff5d2f4393651eb721d008e55071e13c
rusefi <rusefillc@gmail.com>
2020-04-17 12:15:46 -0400

    progress

--- Special/Tools/git_merge_normal_git_into_wiki_git.bat
+++ Special/Tools/git_merge_normal_git_into_wiki_git.bat
@@ -2,7 +2,7 @@ git fetch normal-git                                    >> log.txt 2>>err.txt
 
 git checkout -b wiki-git-local wiki-git/master          >> log.txt 2>>err.txt
 git merge normal-git/master                             >> log.txt 2>>err.txt
-git push wiki-git HEAD:master                           >> log.txt 2>>err.txt
+git push best-wiki-git HEAD:master                      >> log.txt 2>>err.txt
 
 
 git checkout -b best-wiki-git-local wiki-git/master     >> log.txt 2>>err.txt
