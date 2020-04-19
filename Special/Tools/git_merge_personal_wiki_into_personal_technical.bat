f24c2a935505edb2cf5ea9839ec102763c8f1e90
rusefi <rusefillc@gmail.com>
2020-04-19 13:16:03 -0400

    better now?

---
+++ Special/Tools/git_merge_personal_wiki_into_personal_technical.bat
@@ -0,0 +1,4 @@
+
+git checkout master                                               >> log.txt 2>>err.txt
+git merge personal-wiki-git-local                                 >> log.txt 2>>err.txt
+git push                                                          >> log.txt 2>>err.txt
