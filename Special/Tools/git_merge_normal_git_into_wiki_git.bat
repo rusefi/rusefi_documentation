c1989840410d7cd5c8475f2093bf28123cae814e
rusefi <rusefillc@gmail.com>
2020-04-19 14:27:43 -0400

    maybe progress?

--- Special/Tools/git_merge_normal_git_into_wiki_git.bat
+++ Special/Tools/git_merge_normal_git_into_wiki_git.bat
@@ -1,4 +1,6 @@
 git fetch technical-git                                   >> log.txt 2>>err.txt
+git fetch best-wiki-git                                   >> log.txt 2>>err.txt
+git fetch temp-wiki-git                                   >> log.txt 2>>err.txt
 
 git checkout -b best-wiki-git-local best-wiki-git/master  >> log.txt 2>>err.txt
 git merge technical-git/master                            >> log.txt 2>>err.txt
