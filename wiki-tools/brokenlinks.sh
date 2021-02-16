8edddfd9ea102cc99a77214c957e6caf285781ce
David Holdeman <david@chuckwagoncomputing.com>
2021-02-15 20:52:31 -0600

    finish (maybe) script

--- wiki-tools/brokenlinks.sh
+++ wiki-tools/brokenlinks.sh
@@ -13,15 +13,35 @@ searchfile() {
     if echo $link | grep -E '^http' >/dev/null; then
       continue
     fi
-    if echo $link | grep -E ".md$" && find . -name "$link"; then
+    if [ $(find . -name "$link"".md" 2>/dev/null | wc -l) -gt 0 ]; then
       continue
+    fi
+    if echo "$link" | grep "#" >/dev/null; then
+      continue
+    fi
+    if echo "$link" | grep -Ev ".md$" >/dev/null; then
+      if ls "$link" 2>/dev/null >/dev/null; then
+        continue
+      else
+        echo $link
+        FILES=$(find . -iname "*$(basename $link)*")
+        echo "$FILES" | cat --number
+        read PICK
+        FILE=$(echo "$FILES" | head -n $PICK | tail -n 1)
+        REPLACE=$(escape '('$link')')
+        REPLACEWITH=$(escapeReplace "$FILE")
+        sed -i "s/$REPLACE/\($REPLACEWITH\)/" "$1"
+        continue
+      fi
     fi
     echo $link
     FILES=$(find . -iname "*$(basename $link)*")
     echo "$FILES" | cat --number
     read PICK
-    FILE=$(basename $(echo "$FILES" | head -n $PICK | tail -n 1) .md)
-    sed -i "s/$(escape '('$link')')/\($(escapeReplace $FILE)\)/" "$1"
+    FILE=$(basename "$(echo "$FILES" | head -n $PICK | tail -n 1)" .md)
+    REPLACE=$(escape '('$link')')
+    REPLACEWITH=$(escapeReplace "$FILE")
+    sed -i "s/$REPLACE/\($REPLACEWITH\)/" "$1"
   done
 }
 export -f searchfile
