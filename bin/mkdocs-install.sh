558445557704f2bd4215f427b161a0a26aac4e3c
David Holdeman <david@chuckwagoncomputing.com>
2023-01-01 13:00:20 -0600

    Use exclude plugin to allow symlinking instead of copying (#330)
    * install mkdocs-exclude
    
    * add to config
    
    * change doc
    
    * update scripts
    
    * update docs to use scripts
    
    * move sitemap link
    
    * use scripts

--- bin/mkdocs-install.sh
+++ bin/mkdocs-install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install ncftp
 
 # sudo apt install python-pip for 'pip'
 sudo apt install python3-pip
 
 # pip did not work for  in my case?
-pip install mkdocs mkdocs-ezlinks-plugin
+pip install mkdocs mkdocs-ezlinks-plugin mkdocs-exclude
