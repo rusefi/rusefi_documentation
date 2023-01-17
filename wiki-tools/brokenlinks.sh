1d7e02d7ed852271206639d405156814ebdbd369
mi-hol <22799428+mi-hol@users.noreply.github.com>
2023-01-17 19:29:57 +0100

    fix lint warnings

--- wiki-tools/brokenlinks.sh
+++ wiki-tools/brokenlinks.sh
@@ -5,7 +5,9 @@
 #                                   Written By David Holdeman                                    #
 #     Searches for broken links in a Github Wiki repo, and suggests and applies corrections.     #
 ##################################################################################################
-
+# to echo all shell commands called and expand any variable name
+set -o xtrace
+printf '%s\n' "$(date):script started"
 # These two functions are used to escape variables for use in a sed command
 # Passed a single string
 escape() {
@@ -25,25 +27,27 @@ searchfile() {
   # We use file descriptor 3, because if we used stdin, the read calls inside this loop would read from that instead of
   #  reading the user's input.
   while IFS= read -r -u 3 link; do
+    printf '%s\n' "$(date):link:'$link'"
     # If it's an internet link, ignore it.
     # That's beyond the scope of this tool.
-    if echo $link | grep -E '^[http|\/]' >/dev/null; then
+    if echo "$link" | grep -E '^[http|\/]' >/dev/null; then
+      printf '%s\n' "$(date): internet link not checked" 
       continue
     fi
     # At some point in this scripts development, fixed links to files/images were given the './' prefix.
     # This didn't really hurt anything, but it's not idiomatic.
     # I added this to fix the problems I caused, and decided it was worth keeping around.
-    if echo $link | grep -E '^\./' >/dev/null; then
+    if echo "$link" | grep -E '^\./' >/dev/null; then
       # NEWLINK is the corrected link
-      NEWLINK=$(echo $link | sed 's/^\.\///')
+      NEWLINK=$(echo "$link" | sed 's/^\.\///')
       # Print the file and the old link
       echo "In $1:"
-      echo $link
+      echo "$link"
       # Print the options as though they are a list in order to have the same UI as other types of correction
-      echo $NEWLINK | cat --number
+      echo "$NEWLINK" | cat --number
       # Read the user input
-      read PICK
-      if [ $PICK -eq 1 ]; then
+      read -r PICK
+      if [ "$PICK" -eq 1 ]; then
         # Replace the old link with the new one.
         # Parentheses are placed around both the old link and new one in order to ensure we replace the link,
         #   and not some other place in the file that happens to use the same words.
@@ -54,11 +58,12 @@ searchfile() {
       # We don't continue here because the link we fixed might be broken.
     fi
     # Skip links that are to an .md file and aren't broken.
-    if [ $(find . -name "$link"".md" 2>/dev/null | wc -l) -gt 0 ]; then
+    if [ "$(find . -name "$link"".md" 2>/dev/null | wc -l)" -gt 0 ]; then
       continue
     fi
     # Skip links that are to a hash fragment.
     if echo "$link" | grep "#" >/dev/null; then
+      printf '%s\n' "$(date): link to hash fragment not checked" 
       continue
     fi
     # Process links that are not to an .md file.
@@ -72,7 +77,7 @@ searchfile() {
       fi
       # Print the filename and the broken link.
       echo "In $1:"
-      echo $link
+      echo "$link"
       # Build the search term we will look for.
       # All hyphens and underscores are replaced with asterisks, so we
       #   can find files with mismatched hyphens or underscores.
@@ -80,20 +85,20 @@ searchfile() {
       # Search for matching files.
       FILES=$(find . -iname "$SEARCH")
       # If there are no files, skip to next link.
-      if [ $(echo -n "$FILES" | wc -c) -lt 1 ]; then
+      if [ "$(echo -n "$FILES" | wc -c)" -lt 1 ]; then
         echo "Could not find"
         continue
       fi
       # List the potential files, with numbers.
       echo "$FILES" | cat --number
       # Read the user input
-      read PICK
+      read -r PICK
       # If the selection isn't a number, skip to the next link.
       if ! [[ $PICK =~ ^[0-9]+$ ]]; then
         continue
       fi
       # Get the selected file path, without the preceding ./
-      FILE=$(echo "$FILES" | head -n $PICK | tail -n 1 | sed 's/^\.\///')
+      FILE=$(echo "$FILES" | head -n "$PICK" | tail -n 1 | sed 's/^\.\///')
       # Replace the old link with the new one.
       # Parentheses are placed around both the old link and new one in order to ensure we replace the link,
       #   and not some other place in the file that happens to use the same words.
@@ -103,7 +108,7 @@ searchfile() {
       continue
     fi
     echo "In $1:"
-    echo $link
+    echo "$link"
     # Build the search term we will look for.
     # All hyphens and underscores are replaced with asterisks, so we
     #   can find files with mismatched hyphens or underscores.
@@ -111,24 +116,24 @@ searchfile() {
     # Search for matching files.
     FILES=$(find . -iname "$SEARCH")
     # If there are no files, skip to next link.
-    if [ $(echo -n "$FILES" | wc -c) -lt 1 ]; then
+    if [ "$(echo -n "$FILES" | wc -c)" -lt 1 ]; then
       echo "Could not find"
       continue
     fi
     # List the potential files, with numbers.
     echo "$FILES" | cat --number
     # Read the user input
-    read PICK
+    read -r PICK
     # If the selection isn't a number, skip to the next link.
     if ! [[ $PICK =~ ^[0-9]+$ ]]; then
       continue
     fi
     # Get the selected file path, without the preceding ./
-    FILE=$(basename "$(echo "$FILES" | head -n $PICK | tail -n 1)" .md)
+    FILE=$(basename "$(echo "$FILES" | head -n "$PICK" | tail -n 1)" .md)
     # Replace the old link with the new one.
     # Parentheses are placed around both the old link and new one in order to ensure we replace the link,
     #   and not some other place in the file that happens to use the same words.
-    REPLACE=$(escape '('$link')')
+    REPLACE=$(escape '('"$link"')')
     REPLACEWITH=$(escapeReplace "$FILE")
     sed -i "s/$REPLACE/\($REPLACEWITH\)/" "$1"
   # This regex finds links in the file that is passed to searchfile
@@ -138,4 +143,6 @@ searchfile() {
 export -f searchfile
 
 # run searchfile on every .md file in the repo
+printf '%s\n' "$(date)"
 find . -iname "*.md" -exec bash -c 'searchfile "$0"' {} \;
+printf '%s\n' "$(date):script ended"
