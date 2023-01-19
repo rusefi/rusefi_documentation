b38705c569e818ed0ad8d5560194d492400ade0c
David Holdeman <david@chuckwagoncomputing.com>
2023-01-19 15:22:00 -0600

    only run markdownlint if changed

--- wiki-tools/lint.sh
+++ wiki-tools/lint.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 STATUS=0
 CHANGED=$(git diff --name-only HEAD HEAD~1 | grep -v '^_' | grep '.md$')
-if [ "$CHANGED" == "" ]; then
-  exit 0
+if [ -n "$CHANGED" ]; then
+  markdownlint -i '_*' --disable MD033 MD034 MD013 MD024 MD036 -- $CHANGED
 fi
-markdownlint -i '_*' --disable MD033 MD034 MD013 MD024 MD036 -- $CHANGED
 if [ "$?" -gt 0 ]; then
   STATUS=1
 fi
