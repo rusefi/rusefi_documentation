b4eea1d971d357431d2bf383c357356a723d397c
David Holdeman <david@chuckwagoncomputing.com>
2023-01-19 07:32:14 -0600

    run brokenlinks

--- wiki-tools/lint.sh
+++ wiki-tools/lint.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
+STATUS=0
 CHANGED=$(git diff --name-only HEAD HEAD~1 | grep -v '^_' | grep '.md$')
 if [ "$CHANGED" == "" ]; then
   exit 0
 fi
 markdownlint -i '_*' --disable MD033 MD034 MD013 MD024 MD036 -- $CHANGED
+if [ "$?" -gt 0 ]; then
+  STATUS=1
+fi
+
+bash wiki-tools/brokenlinks.sh -s
+if [ "$?" -gt 0 ]; then
+  STATUS=1
+fi
+
+return "$STATUS"
