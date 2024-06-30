0e4486f4c58b42ccc2af7847db1dc006f9ed6755
rusEFI LLC <rusefillc@gmail.com>
2024-06-30 18:46:35 -0400

    no code with us sending a few static packets

--- B6-TCU/send-methods.lua
+++ B6-TCU/send-methods.lua
@@ -1,5 +1,11 @@
+motor5Data = { 0x1C, 0x08, 0xF3, 0x55, 0x19, 0x00, 0x00, 0xAD }
 motor5counter = 0
 motor5FuelCounter = 0
+function sendMotor5()
+	setBitRange(motor5Data, 5, 9, motor5FuelCounter)
+	xorChecksum(motor5Data, 8)
+	txCan(TCU_BUS, MOTOR_5, 0, motor5Data)
+end
 
 canMotorInfo02 = { 0x00, 0x00, 0x00, 0x14, 0x1C, 0x93, 0x48, 0x14 }
 canMotorInfo1 = { 0x99, 0x14, 0x00, 0x7F, 0x00, 0xF0, 0x47, 0x01 }
@@ -23,4 +29,31 @@ function sendMotorInfo()
 	else
 		txCan(TCU_BUS, MOTOR_INFO, 0, canMotorInfo3)
 	end
+end
+
+
+motorBreData = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+motorBreCounter = 0
+function sendMotorBre()
+	motorBreCounter = (motorBreCounter + 1) % 16
+
+	setBitRange(motorBreData, 8, 4, motorBreCounter)
+	xorChecksum(motorBreData, 1)
+
+	txCan(TCU_BUS, MOTOR_BRE, 0, motorBreData)
+end
+
+accGraData = { 0x00, 0x00, 0x08, 0x00, 0x1A, 0x00, 0x02, 0x01 }
+accGraCounter = 0
+function sendAccGra()
+ accGraCounter = (accGraCounter + 1) % 16
+ setBitRange(accGraData, 60, 4, accGraCounter)
+    xorChecksum(accGraData, 1)
+
+ txCan(TCU_BUS, ACC_GRA, 0, accGraData)
+end
+
+motor7Data = { 0x1A, 0x66, 0x7E, 0x00, 0x00, 0x00, 0x00, 0x00 }
+function sendMotor7()
+	txCan(TCU_BUS, MOTOR_7, 0, motor7Data)
 end
