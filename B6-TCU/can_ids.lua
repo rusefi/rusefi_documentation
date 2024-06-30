9c4265c0a5f36ced52594d43b200e0e97ceeba77
rusEFI LLC <rusefillc@gmail.com>
2024-06-30 16:46:07 -0400

    new skeleton progress

--- B6-TCU/can_ids.lua
+++ B6-TCU/can_ids.lua
@@ -1,5 +1,10 @@
 AIRBAG = 0x050
 
+VWTP_OUT = 0x200
+cuType = 0x02 -- TCU
+VWTP_IN = 0x200 + cuType
+VWTP_TESTER = 0x300
+
 -- 640
 MOTOR_1 = 0x280
 -- 644
@@ -19,6 +24,20 @@ MOTOR_INFO = 0x580
 -- 1416
 MOTOR_7 = 0x588
 
+ Bremse_6=424
+ Bremse_5=1192
+
+ Bremse_4=672
+
+ Bremse_3=1184
+ Bremse_2=1440
+ Bremse_1=416
+
+
+Gate_Komf_1 = 912
+Kombi_3 = 1312
+
+Systeminfo_1 = 1488
 
 Diagnose_1 = 2000
 
