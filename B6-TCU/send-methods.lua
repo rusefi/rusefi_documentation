741cf928cabd12c1104a07a498fb44d490d38217
rusEFI LLC <rusefillc@gmail.com>
2024-06-30 18:54:42 -0400

    fake motor_2 happy drive in the air

--- B6-TCU/send-methods.lua
+++ B6-TCU/send-methods.lua
@@ -57,3 +57,24 @@ motor7Data = { 0x1A, 0x66, 0x7E, 0x00, 0x00, 0x00, 0x00, 0x00 }
 function sendMotor7()
 	txCan(TCU_BUS, MOTOR_7, 0, motor7Data)
 end
+
+motor2mux = { 0x8A, 0xE8, 0x2C, 0x64 }
+motor2Data = { 0x8A, 0x8D, 0x10, 0x04, 0x00, 0x4C, 0xDC, 0x87 }
+motor2counter = 0
+function sendMotor2()
+ motor2counter = (motor2counter + 1) % 16
+
+    minTorque = fakeTorque / 2
+    -- todo: add CLT
+    motor2Data[7] = math.floor(minTorque / 0.39)
+
+--print ( "brake " .. getBitRange(data, 16, 2) .. " " .. rpm)
+
+    brakeBit = rpm < 2000 and 1 or 0
+    setBitRange(motor2Data, 16, 1, brakeBit)
+
+    index = math.floor(motor2counter / 4)
+    motor2Data[1] = motor2mux[1 + index]
+
+ txCan(TCU_BUS, MOTOR_2_648, 0, motor2Data)
+end
