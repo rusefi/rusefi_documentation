471bf37067811a0411f64cae464390ea2561c219
rusefillc <rusefillc@gmail.com>
2025-11-12 08:33:21 -0500

    dead comment

--- bin/mkdocs-install.sh
+++ bin/mkdocs-install.sh
@@ -5,5 +5,4 @@ sudo apt update
 # sudo apt install python-pip for 'pip'
 sudo apt install python3-pip
 
-# pip did not work for  in my case?
 pip install mkdocs mkdocs-ezlinks-plugin mkdocs-exclude pymdown-extensions
