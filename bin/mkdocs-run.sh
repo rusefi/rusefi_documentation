31fbf3435ee649b1f6b33f99392825f16b9e12f8
David Holdeman <dave@holdemanenterprises.com>
2025-11-20 19:33:57 +0000

    Switch to zensical

--- bin/mkdocs-run.sh
+++ bin/mkdocs-run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+shopt -s extglob
 cd mkdocs
-ln -s .. docs
-cp style.css ..
-mkdocs build --strict
+mkdir -p docs
+cp -t docs -r ../!(mkdocs|wiki-tools|bin|_*)
+cp style.css docs
+zensical build
+rm -r docs
 if [ $? -ne 0 ]; then
 	exit 1
 fi
-cd ..
-rm style.css
