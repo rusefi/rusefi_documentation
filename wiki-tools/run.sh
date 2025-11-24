6c91c17dc24f148c4b316285c5da759e67d81553
David Holdeman <dave@holdemanenterprises.com>
2025-11-24 03:27:35 +0000

    Undo error-hiding mistake

--- wiki-tools/run.sh
+++ wiki-tools/run.sh
@@ -5,10 +5,11 @@ mkdir -p docs
 cp -t docs -r ../!(generator|wiki-tools|_*)
 cp style.css docs
 zensical build
-rm -r docs
 if [ $? -ne 0 ]; then
+	rm -r docs
 	exit 1
 fi
+rm -r docs
 cd ..
 bash wiki-tools/genpdf.sh \
 		 HOWTO-quick-start.md \
