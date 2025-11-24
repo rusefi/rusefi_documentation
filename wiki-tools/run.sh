4fe055a8c3bf2f1f6d0b5be462cef8e311c828b3
David Holdeman <dave@holdemanenterprises.com>
2025-11-24 01:54:20 +0000

    Generate PDF when deploying wiki

--- wiki-tools/run.sh
+++ wiki-tools/run.sh
@@ -9,3 +9,9 @@ rm -r docs
 if [ $? -ne 0 ]; then
 	exit 1
 fi
+cd ..
+bash wiki-tools/genpdf.sh \
+		 HOWTO-quick-start.md \
+		 HOWTO-create-tunerstudio-project.md \
+		 HOWTO-Get-Running.md \
+		 HOWTO-Get-Running-PnP.md
